0bd6f7224c4beec6d442d0f50c3525cfaf550b18
reinSPQR <rein1605@bvm.network>
2025-10-23 10:20:29 +0700

    Update test script

--- scripts/test_payload.py
+++ scripts/test_payload.py
@@ -1,4 +1,6 @@
+import base64
 import requests
+import json
 
 def main():
     url = "http://localhost:8000/v1/audios/generate"
@@ -7,8 +9,34 @@ def main():
         "input_text": "Hello, how are you?",
         "voice_type": "lumira"
     }
-    response = requests.post(url, json=data)
-    print(response.json())
+
+    audio_base64 = ""
+    final_response = ""
+
+    response = requests.post(url, json=data, stream=True)
+    for chunk in response.iter_content(chunk_size=1024):
+        if chunk:
+            chunk_str = chunk.decode("utf-8")
+            if chunk_str.startswith("data: "):
+                current_chunk = chunk_str.split("data: ")[1].strip()
+            else:
+                current_chunk = current_chunk + chunk_str
+                        
+            if current_chunk == "[DONE]":
+                break
+            
+            try:
+                data = json.loads(current_chunk)
+                final_response += data["content"]
+                audio_base64 += data["audio_base64"]
+            except json.JSONDecodeError:
+                pass
+
+    print("Final response:", final_response)
+    
+    with open("temp_audio.wav", "wb") as f:
+        f.write(base64.b64decode(audio_base64))
+
 
 if __name__ == "__main__":
     main()
